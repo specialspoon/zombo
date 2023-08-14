8ae4aacce80257af282bef2fd1abbc4ddf9ffb91
Ice_creme <141097782+ice-creme-studios@users.noreply.github.com>
2023-08-14 17:30:26 +1200

    added new animations

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -16,6 +16,7 @@ public class PlayerMovement : MonoBehaviour
     public SpriteRenderer gunSprite;
 
     public float gunRot;
+    public bool isMoving = false;
 
     Vector2 movement;
 
@@ -30,31 +31,70 @@ public class PlayerMovement : MonoBehaviour
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
 
         gunRot = gunTransform.eulerAngles.z;
-        if (gunRot > 240 && gunRot < 315)
-        {
-            animator.Play("player_walk_right");
-            gunSprite.flipX = false;
-            gunSprite.sortingOrder = 1;
-        }
-        if (gunRot > 120 && gunRot < 240){
-            animator.Play("player_walk_down");
-            gunSprite.sortingOrder = 1;
-        }
-        if (gunRot > 30 && gunRot < 120)
-        {
-            animator.Play("player_walk_left");
-            gunSprite.sortingOrder = 1;
-            gunSprite.flipX = true;
-        }
-        if (gunRot > 315 || gunRot < 30)
-        {
-            animator.Play("player_walk_up");
-            gunSprite.sortingOrder = -1;
-        }
+
+        Debug.Log(movement.x != 0 || movement.y != 0);
+        Animate();
     }
 
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    public void Animate()
+    {
+ 
+
+        if (!isMoving)
+        {
+            if (gunRot > 240 && gunRot < 315)
+            {
+                animator.Play("player_look_right");
+                gunSprite.flipX = false;
+                gunSprite.sortingOrder = 1;
+            }
+            if (gunRot > 120 && gunRot < 240)
+            {
+                animator.Play("player_look_down");
+                gunSprite.sortingOrder = 1;
+            }
+            if (gunRot > 30 && gunRot < 120)
+            {
+                animator.Play("player_look_left");
+                gunSprite.sortingOrder = 1;
+                gunSprite.flipX = true;
+            }
+            if (gunRot > 315 || gunRot < 30)
+            {
+                animator.Play("player_look_up");
+                gunSprite.sortingOrder = -1;
+            }
+        }
+
+        if (isMoving)
+        {
+            if (gunRot > 240 && gunRot < 315)
+            {
+                animator.Play("player_walk_right");
+                gunSprite.flipX = false;
+                gunSprite.sortingOrder = 1;
+            }
+            if (gunRot > 120 && gunRot < 240)
+            {
+                animator.Play("player_walk_down");
+                gunSprite.sortingOrder = 1;
+            }
+            if (gunRot > 30 && gunRot < 120)
+            {
+                animator.Play("player_walk_left");
+                gunSprite.sortingOrder = 1;
+                gunSprite.flipX = true;
+            }
+            if (gunRot > 315 || gunRot < 30)
+            {
+                animator.Play("player_walk_up");
+                gunSprite.sortingOrder = -1;
+            }
+        }
     }
 }
